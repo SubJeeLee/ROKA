a78ab6d80bddac465f787d654eb842911b9f8afc
JUNSUBLEE <leejs010605@naver.com>
2024-10-16 14:35:26 +0000

    feat: Header badges

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -7,11 +7,38 @@ searchEl.addEventListener('click', function () {
 })
 // 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
 searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused')
-    searchInputEl.setAttribute('placeholder', '통합검색')
-  })
-  // 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-  searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused')
-    searchInputEl.setAttribute('placeholder', '')
-  })
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
+
+
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300))
