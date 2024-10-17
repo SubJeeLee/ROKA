1328ec915c527d31e1bc0c478350d105dc4365c3
JUNSUBLEE <leejs010605@naver.com>
2024-10-17 14:31:58 +0000

    feat: Fade-in

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -42,3 +42,18 @@ window.addEventListener('scroll', _.throttle(function () {
     })
   }
 }, 300))
+
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach((fadeEl, index) => {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+})
